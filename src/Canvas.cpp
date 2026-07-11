614695068961f47dc873cd19e0aef79311df4b23
YahavMarom <marom.yahav1@gmail.com>
2026-07-11 13:08:07 +0300

    added Canvas

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
-#include "Tuple.h"
+#include "Canvas.h"
+
+
+Canvas::Canvas(int width, int height)
+    : m_width {width}
+    , m_height {height}
+    , m_pixels {static_cast<std::vector<Color>::size_type>(width * height), Color(0,0,0) }
+    {}
+
+void Canvas::writePixel(int row, int col, const Color& color) {
+    m_pixels[row * m_width + col] = color;
+}
+
+Color Canvas::pixelAt(int row, int col) const{
+    return m_pixels[row * m_width + col];
+}
+
---
+++ src/Color.cpp
@@ -0,0 +1,48 @@
+#include "Color.h"
+#include "cassert"
+
+Color::Color(double red, double green, double blue)
+    : m_red {red}
+    , m_green {green}
+    , m_blue {blue}
+    {
+    assert( (m_red >= 0 & m_red <= 1)
+            & (m_green >= 0 & m_green <= 1)
+            & (m_blue >= 0 & m_blue <= 1));
+    }
+
+Color operator+(const Color& a, const Color& b) {
+    return Color(
+        a.getRed() + b.getRed(),
+        a.getGreen() + b.getGreen(),
+        a.getBlue() + b.getBlue()
+    );
+}
+
+Color operator-(const Color& a, const Color& b) {
+    return Color(
+        a.getRed() - b.getRed(),
+        a.getGreen() - b.getGreen(),
+        a.getBlue() - b.getBlue()
+    );
+}
+
+Color operator*(double const scalar, const Color& c) {
+    return Color(
+        scalar * c.getRed(),
+        scalar * c.getGreen(),
+        scalar * c.getBlue()
+    );
+}
+
+Color operator*(const Color& c, double const scalar) {
+    return scalar * c;
+}
+
+Color operator*(const Color& a, const Color& b) {
+    return Color(
+        a.getRed() * b.getRed(),
+        a.getGreen() * b.getGreen(),
+        a.getBlue() * b.getBlue()
+    );
+}
